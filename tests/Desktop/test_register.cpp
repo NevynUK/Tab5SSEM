870e24c7edee30b2fcb9cf503158db0ec2b80187
Mark Stevens <mark@thepcsite.co.uk>
2026-04-13 10:10:31 +0100

    Added some tests

---
+++ tests/Desktop/test_register.cpp
@@ -0,0 +1,84 @@
+#include "Register.hpp"
+
+/**
+ * @brief Test the register class.
+ * 
+ * @return true Register tests have passed.
+ * @return false Register tests have failed.
+ */
+bool TestRegister()
+{
+    Register register1;
+    Register register2(0x12345678);
+
+    if (register1.GetValue() != 0)
+    {
+        printf("Register1 is not zero.\n");
+        return(false);
+    }
+    register1.Increment();
+    if (register1.GetValue() != 1)
+    {
+        printf("Register1 did not increment correctly.\n");
+        return(false);
+    }
+    register1.Add(9);
+    if (register1.GetValue() != 10)
+    {
+        printf("Register1 did not add a constant correctly.\n");
+        return(false);
+    }
+    Register addValue(45);
+    register1.Add(addValue);
+    if (register1.GetValue() != 55)
+    {
+        printf("Register1 did not add another register correctly.\n");
+        return(false);
+    }
+    register1.Subtract(addValue);
+    if (register1.GetValue() != 10)
+    {
+        printf("Register1 did not subtract another register correctly.\n");
+        return(false);
+    }
+
+    register1.Negate();
+    if (register1.GetValue() != -10)
+    {
+        printf("Register1 did not negate correctly.\n");
+        return(false);
+    }
+
+    register1.Negate();    
+    if (register1.GetValue() != 10)
+    {
+        printf("Register1 did not revert back to 10.\n");
+        return(false);
+    }
+
+    string binary = register1.Binary();
+    if (binary != "01010000000000000000000000000000")
+    {
+        printf("Register1 did not convert to binary correctly, result '%s'.\n", binary.c_str());
+        return(false);
+    }
+
+    if (register1.LineNumber() != 10)
+    {
+        printf("Register1 did not return the correct line number.\n");
+        return(false);
+    }
+
+    if (register2.GetValue() != 0x12345678)
+    {
+        return(false);
+    }
+
+    register1.SetValue(0x87654321);
+    if (((uint32_t) register1.GetValue()) != 0x87654321)
+    {
+        return(false);
+    }
+
+    return(true);
+}
